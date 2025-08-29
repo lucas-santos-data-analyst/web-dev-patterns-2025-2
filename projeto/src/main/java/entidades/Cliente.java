bc076a1a80fa88597a97d7d5acb22a08c8639bf6
Lucas Adriano <145597348+lucas-santos-data-analyst@users.noreply.github.com>
2025-08-29 01:41:49 +0000

    Aula280825

---
+++ projeto/src/main/java/entidades/Cliente.java
@@ -0,0 +1,17 @@
+package entidades;
+
+import io.quarkus.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int codigo;
+
+    String nome;
+    String cpf;
+}
