76f3782d5dfac6c18f94eb7a9db8eec4ab454bab
Lucas Adriano <145597348+lucas-santos-data-analyst@users.noreply.github.com>
2025-08-29 00:49:26 +0000

    Aula250825

---
+++ projeto/src/main/java/entidades/Categoria.java
@@ -0,0 +1,32 @@
+// sudo su modo superusuario enquanto utiliar
+// rodar projeto quarkus mvn quarkus:dev
+
+package entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer codigo;
+    
+    public String nome;
+
+}
+
+/*
+ * 
+ * # Criar novo usuario no mysql
+# create user lucas@localhost identified by 'Unifio@123';
+#
+# Dar permissão de escrita no mysql
+# grant all on *.* to lucas@localhost
+#
+#
+#
+#
+ */
